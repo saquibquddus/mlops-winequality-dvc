6a409f5ede51f34d4ecd9735090a31f8e8aa1915
saquibquddus <sakibquddus@gmail.com>
2021-12-12 10:51:00 +0530

    update app.py

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ def index():
 
         except Exception as e:
             print(e)
-            # error = {"error": "Something went wrong!! Try again"}
-            return render_template("404.html", error=e)
+            error = {"error": "Something went wrong!! Try again"}
+            error= {"error":e}
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
