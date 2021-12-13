d83ef9a3b3baff9a58446607a229e603d8acc441
saquibquddus <sakibquddus@gmail.com>
2021-12-13 10:36:17 +0530

    column name issue

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -74,6 +74,15 @@ def api_response(dict_request):
             response = predict(data)
             response = {"response": response}
             return response
-    except Exception as e:
+    
+    except NotInRange as e:
         response = {"the_expected_range": get_schema(), "response": str(e)}
+        return response
+
+    except NotInCols as e:
+        response = {"the_expected_cols": get_schema().keys(), "response": str(e)}
+        return response
+
+    except Exception as e:
+        response = {"response": str(e)}
         return response
